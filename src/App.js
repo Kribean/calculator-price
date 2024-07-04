f16f0770f806e007b31a7b07257e96e4f7cfb0d7
Kribean <caraibe.simulation@gmail.com>
2024-07-05 00:39:12 +0200

    feat: fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import PriceCalculator from './Component/PriceCalculator';
+import "./App.css";
+import PriceCalculator from "./Component/PriceCalculator";
 
 function App() {
   return (
     <div className="w-full p-4">
-<PriceCalculator/>
+      <PriceCalculator />
     </div>
   );
 }
