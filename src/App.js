a99fe017cf50ec9dc29694c99feca00256fb88a1
Kribean <caraibe.simulation@gmail.com>
2024-07-08 18:58:59 +0200

    fix: remove useless component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import GoogleFormEmbed from "./Component/GoogleFormEmbed";
 import PriceCalculator from "./Component/PriceCalculator";
 
 function App() {
   return (
     <div className="w-full p-4">
       <PriceCalculator />
-      <GoogleFormEmbed/>
     </div>
   );
 }
